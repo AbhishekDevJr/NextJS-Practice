a7873b39460ed6edce9765718b0ce097aadc62dc
Abhishek Choudhari <abhishek007coc@gmail.com>
2024-03-25 00:05:44 +0530

    Learnt to implement Custom Not-Found Page, Dynamic NotFound Page for specific routes when notFound Function is called

---
+++ next-js-practice/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <>
+            <h1>Specified Route Not Found.</h1>
+            <p>Requested Resource/Requested Route could not be found.</p>
+        </>
+    );
+}
--- next-js-practice/src/app/products/[productid]/reviews/[reviewId]/page.tsx
+++ next-js-practice/src/app/products/[productid]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default function ProductReview({ params }: {
     params: {
         productid: string,
         reviewId: string
     }
 }) {
+    if (Number(params.reviewId) > 1000) {
+        notFound();
+    }
     return (
         <div className=''>
             Review {params.reviewId} of Product: {params.productid}
