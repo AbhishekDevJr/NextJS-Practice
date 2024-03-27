7cfc48effa2497fdc4a662a650d5112559b03848
Abhishek Choudhari <abhishek007coc@gmail.com>
2024-03-28 00:02:14 +0530

    Implemented Dynamic Metadata using Metadata type, generateMetadata function

--- next-js-practice/src/app/products/[productid]/page.tsx
+++ next-js-practice/src/app/products/[productid]/page.tsx
@@ -1,8 +1,19 @@
-export default function ProductDetail({ params }: {
+import { Metadata } from "next";
+
+type Props = {
     params: {
         productid: string
     }
-}) {
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+    return {
+        title: `Product-ID: ${params.productid}`,
+        description: `Product-Description: ${params.productid}`
+    };
+}
+
+export default function ProductDetail({ params }: Props) {
     return (
         <div className=''>
             Product Details {params.productid} Component
