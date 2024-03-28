7292c005fa7e21e31dca540b3d3970c8be2cc1ca
Abhishek Choudhari <abhishek007coc@gmail.com>
2024-03-29 00:01:17 +0530

    Handling Errors using Error.tsx

---
+++ next-js-practice/src/app/products/[productid]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,7 @@
+'use client';
+export default function ErrorBoundary() {
+    return (
+        <h1>
+            Error in Loading this Section. Please try again.
+        </h1>);
+}
--- next-js-practice/src/app/products/[productid]/reviews/[reviewId]/page.tsx
+++ next-js-practice/src/app/products/[productid]/reviews/[reviewId]/page.tsx
@@ -9,6 +9,11 @@ export default function ProductReview({ params }: {
     if (Number(params.reviewId) > 1000) {
         notFound();
     }
+
+    if (Math.floor(Math.random() * 2) === 1) {
+        throw new Error('New Error!');
+    }
+
     return (
         <div className=''>
             Review {params.reviewId} of Product: {params.productid}
