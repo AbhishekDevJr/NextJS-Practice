6a22e8c77b5e592da331a378ac79dcb98f0633a5
Abhishek Choudhari <abhishek007coc@gmail.com>
2024-03-28 23:08:58 +0530

    Added Metadata with absolute, default, template properties

--- next-js-practice/src/app/post/first/page.tsx
+++ next-js-practice/src/app/post/first/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'First Post',
+    description: 'Description about 1st Post'
+}
+
 export default function First() {
     return (
         <div className=''>
