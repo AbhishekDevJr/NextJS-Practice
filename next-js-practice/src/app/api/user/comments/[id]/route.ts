7552193854b6ab8d3f5ab3fc579613da606477f8
Abhishek Choudhari <abhishek007coc@gmail.com>
2024-04-01 22:18:40 +0530

    Implemented PATCH Method using Dynamic Route-handler

--- next-js-practice/src/app/api/user/comments/[id]/route.ts
+++ next-js-practice/src/app/api/user/comments/[id]/route.ts
@@ -4,3 +4,11 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
     const comment = comments.find((item) => item.id === Number(params.id));
     return Response.json(comment);
 }
+
+export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+    const reqUp = await req.json();
+    const { text } = reqUp;
+    const index = comments.findIndex(item => item.id === Number(params.id));
+    comments[index].comments = text;
+    return Response.json(comments[index]);
+}
